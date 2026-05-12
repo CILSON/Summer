da367f8e3cb8c3cb7d04e35290cec99e343bb385
CILSON <siddarth.cilson@gmail.com>
2026-05-12 13:57:03 +0530

    drf

--- Data/bybit.py
+++ Data/bybit.py
@@ -10,24 +10,50 @@ def fetch_historical_data1(symbol='BTCUSDT', timeframe="1M"):
         session = HTTP(testnet=False)
         data = session.get_kline(
             category="spot",
-            symbol="BTCUSDT",
-            interval="60"
+            symbol=symbol,
+            interval=timeframe
         )
 
         data = data['result']['list']
 
-        df = pd.DataFrame(data, columns=['OpenTime', 'Open', 'High', 'Low', 'Close', 'Volume', 'Quote Asset Volume'])
+        if not data or len(data) < 2:
+            return pd.DataFrame()
 
-        columns_to_convert = ['OpenTime','Open', 'High', 'Low', 'Close', 'Volume', 'Quote Asset Volume']
+        df = pd.DataFrame(data, columns=[
+            'OpenTime',
+            'Open',
+            'High',
+            'Low',
+            'Close',
+            'Volume',
+            'Quote Asset Volume'
+        ])
+
+        columns_to_convert = [
+            'OpenTime',
+            'Open',
+            'High',
+            'Low',
+            'Close',
+            'Volume',
+            'Quote Asset Volume'
+        ]
+
         df[columns_to_convert] = df[columns_to_convert].apply(pd.to_numeric)
 
         df['OpenTime'] = pd.to_datetime(df['OpenTime'], unit='ms')
-        df['OpenTime'] = df['OpenTime'].dt.tz_localize('UTC').dt.tz_convert('Asia/Kolkata').dt.tz_localize(None)
-        
+
+        df['OpenTime'] = (
+            df['OpenTime']
+            .dt.tz_localize('UTC')
+            .dt.tz_convert('Asia/Kolkata')
+            .dt.tz_localize(None)
+        )
+
         df = df.sort_values('OpenTime', ascending=False).reset_index(drop=True)
 
         return df
 
     except Exception as e:
-        print(e)
+        print("Bybit Error:", e)
         return pd.DataFrame()
